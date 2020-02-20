0451eed80e48afc190146b6bdf4a3df5cedc86b7
anu-ujin <anu.b@nmma.co>
2020-02-20 17:24:13 +0800

    fix investors

--- src/pages/directors.js
+++ src/pages/directors.js
@@ -11,122 +11,84 @@ class Team extends Component {
 
         <div className="team-business our-team pt-150 agn-about-us">
           <img src="images/shape/shape-61.svg" alt="" className="shape-one"/>
-          <img src="images/shape/shape-33.svg" alt="" className="shape-two" data-aos="fade-left"/>
-          <img src="images/shape/shape-65.svg" alt="" className="shape-three" data-aos="fade-right"/>
-          <img src="images/shape/shape-60.svg" alt="" className="shape-four"/>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6">
-                <div className="single-team-member">
-                  <div className="wrapper d-sm-flex">
-                    <div className="img-box">
-                      <img src="images/investors/otgonbat.jpg" alt=""/>
-                    </div>
-                    <div className="info-meta">
-                      <h6 className="name"><strong>Cэдбазарын Отгонбат</strong></h6>
-                      <span>Аврагас Трейд ХХК-ийн Ерөнхий Захирал</span>
-                      <ul>
-                        <li><a href="https://www.facebook.com/otgonbat.sedbazar" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="single-team-member">
-                  <div className="wrapper d-sm-flex">
-                    <div className="img-box">
-                      <img src="images/investors/batsaikhan.jpg" alt=""/>
-                    </div>
-                    <div className="info-meta">
-                      <h6 className="name"><strong>Пүрэвийн Батсайхан</strong></h6>
+  				<img src="images/shape/shape-33.svg" alt="" className="shape-two" data-aos="fade-left"/>
+  				<img src="images/shape/shape-65.svg" alt="" className="shape-three" data-aos="fade-right"/>
+  				<img src="images/shape/shape-60.svg" alt="" className="shape-four"/>
+  				<div className="container">
+  					<div className="row">
+  						<div className="col-lg-6">
+  							<div className="single-team-member">
+  								<div className="wrapper d-sm-flex">
+  									<div className="img-box">
+  										<img src="images/investors/otgonbat.jpg" alt=""/>
+  									</div>
+  									<div className="info-meta">
+  										<h6 className="name"><strong>Cэдбазарын Отгонбат</strong></h6>
+  										<span>Аврагас Трейд ХХК-ийн Ерөнхий Захирал</span>
+                      <q>Төлөөлөн Удирдах Зөвлөлийн Дарга</q>
+  									</div>
+  								</div>
+  							</div>
+  						</div>
+  						<div className="col-lg-6">
+  							<div className="single-team-member">
+  								<div className="wrapper d-sm-flex">
+  									<div className="img-box">
+  										<img src="images/investors/batsaikhan.jpg" alt=""/>
+  									</div>
+  									<div className="info-meta">
+  										<h6 className="name"><strong>Пүрэвийн Батсайхан</strong></h6>
                       <span>Шунхлай Группын Ерөнхийлөгч</span>
-                      <ul>
-                        <li><a href="https://www.facebook.com/P.Bagi" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="single-team-member">
-                  <div className="wrapper d-sm-flex">
-                    <div className="img-box">
-                      <img src="images/investors/damjin.jpg" alt=""/>
-                    </div>
-                    <div className="info-meta">
-                      <h6 className="name"><strong>Одонгийн Дамжин</strong></h6>
+  										<q>Төлөөлөн Удирдах Зөвлөлийн Гишүүн</q>
+  									</div>
+  								</div>
+  							</div>
+  						</div>
+  						<div className="col-lg-6">
+  							<div className="single-team-member">
+  								<div className="wrapper d-sm-flex">
+  									<div className="img-box">
+  										<img src="images/investors/damjin.jpg" alt=""/>
+  									</div>
+  									<div className="info-meta">
+  										<h6 className="name"><strong>Одонгийн Дамжин</strong></h6>
                       <span>ТЭСО Корпорацийн Төлөөлөн Удирдах Зөвлөлийн Дарга</span>
-                      <ul>
-                        <li><a href="https://www.facebook.com/odon.damjin" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="single-team-member">
-                  <div className="wrapper d-sm-flex">
-                    <div className="img-box">
-                      <img src="images/investors/altanbagana.jpg" alt=""/>
-                    </div>
-                    <div className="info-meta">
-                      <h6 className="name"><strong>Шийтуугийн Алтанбагана</strong></h6>
+  										<q>Төлөөлөн Удирдах Зөвлөлийн Гишүүн</q>
+  									</div>
+  								</div>
+  							</div>
+  						</div>
+  						<div className="col-lg-6">
+  							<div className="single-team-member">
+  								<div className="wrapper d-sm-flex">
+  									<div className="img-box">
+  										<img src="images/investors/altanbagana.jpg" alt=""/>
+  									</div>
+  									<div className="info-meta">
+  										<h6 className="name"><strong>Шийтуугийн Алтанбагана</strong></h6>
                       <span>Тера Экспресс ХХК-ийн Ерөнхий Захирал</span>
-                      <ul>
-                        <li><a href="https://www.facebook.com/baganaa.sh" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="single-team-member">
-                  <div className="wrapper d-sm-flex">
-                    <div className="img-box">
-                      <img src="images/investors/mendorshikh.jpg" alt=""/>
-                    </div>
-                    <div className="info-meta">
-                      <h6 className="name"><strong>Амартайваны Мэнд-Орших</strong></h6>
+  										<q>Төлөөлөн Удирдах Зөвлөлийн Гишүүн</q>
+  									</div>
+  								</div>
+  							</div>
+  						</div>
+  						<div className="col-lg-6">
+  							<div className="single-team-member">
+  								<div className="wrapper d-sm-flex">
+  									<div className="img-box">
+  										<img src="images/investors/mendorshikh.jpg" alt=""/>
+  									</div>
+  									<div className="info-meta">
+  										<h6 className="name"><strong>Амартайваны Мэнд-Орших</strong></h6>
                       <span>Эрксис Монголиа ХХК-ийн Гүйцэтгэх Захирал</span>
-                      <ul>
-                        <li><a href="https://www.facebook.com/MendOrshikh/" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="https://twitter.com/MendOrshikh" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="single-team-member">
-                  <div className="wrapper d-sm-flex">
-                    <div className="img-box">
-                      <img src="images/investors/batka.jpg" alt=""/>
-                    </div>
-                    <div className="info-meta">
-                      <h6 className="name"><strong>Ганхуягийн Бат-Эрдэнэ</strong></h6>
-                      <span>Монгол HD Телевизийн Дэд Ерөнхийлөгч</span>
-                      <ul>
-                        <li><a href="https://www.facebook.com/batkag" target="_blank"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  										<q>Төлөөлөн Удирдах Зөвлөлийн Гишүүн</q>
+  									</div>
+  								</div>
+  							</div>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
       </Layout>
     )
   }
