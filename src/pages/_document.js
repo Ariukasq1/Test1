234151c553e3749ffd4cf37cb24dbf95e9c280f6
Buyantogtokh <buyantogtokh.s@nmma.co>
2020-05-05 16:27:56 +0800

    remove duplicate gtm loader script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,14 +10,6 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KBQC2JN');`,
-          }}>
-          </script>
           <style>{`body { margin: 0 } /* custom! */`}</style>
         </Head>
         <body className="custom_class">
