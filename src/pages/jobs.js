adbfc026c1ffa4b865af27f92e3a9d39bb997537
anu-ujin <anu.b@nmma.co>
2020-06-11 22:35:18 +0800

    fix some styling in jobs

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import WPAPI from 'wpapi';
 import Config from "../config";
@@ -16,7 +16,7 @@ class HumanResource extends Component {
       .embed();
 
     return { posts };
-	}
+  }
 
   render() {
     const { posts } = this.props;
@@ -27,39 +27,39 @@ class HumanResource extends Component {
           <h2 className="page-title">Нээлттэй ажлын байр</h2>
         </div>
 
-        <div className="faq-page faq-tab-wrapper pt-150 pb-200">
-  				<div className="container">
-  					<div className="row">
-  						<div className="col-lg-12">
-  				      <div className="faq-panel">
-  								<div className="panel-group theme-accordion" id="accordion">
+        <div className="jobs faq-page faq-tab-wrapper pt-150 pb-200">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="faq-panel">
+                  <div className="panel-group theme-accordion" id="accordion">
                     {posts.map(post => {
                       return (
                         <div key={post.id} className="panel">
-      								    <div className="panel-heading">
-      								      <h6 className="panel-title">
-      								        <a data-toggle="collapse" data-parent="#accordion" href={`#${post.id}`}>
-      								        {post.title.rendered} <span>
-                              <i className="fa fa-calendar" aria-hidden="true"></i> {moment(post.date).format('L')}</span></a>
-      								      </h6>
-      								    </div>
-      								    <div id={post.id} className="panel-collapse collapse">
+                          <div className="panel-heading">
+                            <h6 className="panel-title">
+                              <a data-toggle="collapse" data-parent="#accordion" href={`#${post.id}`}>
+                                {post.title.rendered} <span>
+                                  <i className="fa fa-calendar" aria-hidden="true"></i> {moment(post.date).format('L')}</span></a>
+                            </h6>
+                          </div>
+                          <div id={post.id} className="panel-collapse collapse">
                             <div className="panel-body">
                               <div className="panel-row pt-30 pb-30">
                                 <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
                                 <div className="pt-30"><button className="line-button-one">Анкет бөглөх</button></div>
                               </div>
                             </div>
-      								    </div>
-      								  </div>
+                          </div>
+                        </div>
                       );
                     })}
-  								</div>
-  							</div>
-  						</div>
-  					</div>
-  				</div>
-  			</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Layout>
     )
   }
