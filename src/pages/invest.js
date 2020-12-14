cf2314188ad6412eb6330b81940d2cf9023ad57f
anu-ujin <anu.b@nmma.co>
2020-12-14 13:39:47 +0800

    remove some console error

--- src/pages/invest.js
+++ src/pages/invest.js
@@ -277,10 +277,10 @@ class Invest extends Component {
                 width="800"
                 height="450"
                 scrolling="no"
-                frameborder="0"
-                webkitallowfullscreen
-                mozallowfullscreen
-                allowfullscreen
+                frameBorder="0"
+                webkitallowfullscreen="true"
+                mozallowfullscreen="true"
+                allowFullScreen={true}
               ></iframe>
             </div>
           </div>
@@ -289,35 +289,35 @@ class Invest extends Component {
           <div className="shape-wrapper">
             <svg className="img-shape shape-one">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 fill="rgb(255, 223, 204)"
                 d="M6.000,12.000 C9.314,12.000 12.000,9.314 12.000,6.000 C12.000,2.686 9.314,-0.000 6.000,-0.000 C2.686,-0.000 -0.000,2.686 -0.000,6.000 C-0.000,9.314 2.686,12.000 6.000,12.000 Z"
               />
             </svg>
             <svg className="img-shape shape-two">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 fill="rgb(182, 255, 234)"
                 d="M6.000,12.000 C9.314,12.000 12.000,9.314 12.000,6.000 C12.000,2.686 9.314,-0.000 6.000,-0.000 C2.686,-0.000 -0.000,2.686 -0.000,6.000 C-0.000,9.314 2.686,12.000 6.000,12.000 Z"
               />
             </svg>
             <svg className="img-shape shape-three">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 fill="rgb(181, 198, 255)"
                 d="M12.000,24.000 C18.627,24.000 24.000,18.627 24.000,12.000 C24.000,5.372 18.627,-0.000 12.000,-0.000 C5.372,-0.000 -0.000,5.372 -0.000,12.000 C-0.000,18.627 5.372,24.000 12.000,24.000 Z"
               />
             </svg>
             <svg className="img-shape shape-four">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 fill="rgb(255, 156, 161)"
                 d="M7.500,15.000 C11.642,15.000 15.000,11.642 15.000,7.500 C15.000,3.358 11.642,-0.000 7.500,-0.000 C3.358,-0.000 -0.000,3.358 -0.000,7.500 C-0.000,11.642 3.358,15.000 7.500,15.000 Z"
               />
             </svg>
             <svg className="img-shape shape-five">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 fill="rgb(178, 236, 255)"
                 d="M12.500,25.000 C19.403,25.000 25.000,19.403 25.000,12.500 C25.000,5.596 19.403,-0.000 12.500,-0.000 C5.596,-0.000 -0.000,5.596 -0.000,12.500 C-0.000,19.403 5.596,25.000 12.500,25.000 Z"
               />
