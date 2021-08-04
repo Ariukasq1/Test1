b08017129cad1949f40111218a4a2c9a5c41a3f4
nandirb <nandir.be@gmail.com>
2021-08-04 16:57:29 +0800

    add google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,17 +5,40 @@ export default class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
+ 
 
   render() {
     return (
       <html>
         <Head>
           <style>{`body { margin: 0 } /* custom! */`}</style>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MYMMCL8CRP"></script>
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MYMMCL8CRP');
+            `,
+          }}
+        />
+         
+
         </Head>
+        
         <body className='custom_class'>
           <Main />
           <NextScript />
+
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBQC2JN"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}></noscript>
+
         </body>
+        
       </html>
     );
   } // end render()
